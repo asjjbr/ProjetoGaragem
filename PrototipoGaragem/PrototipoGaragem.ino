3c41b4ea9f745079820a2a8df78cae44f4a2ee04
Sidnei de Souza Martins <ssmartins@gmail.com>
2017-10-17 21:14:10 -0200

    Criada função para acender, apagar e comutar um led

--- PrototipoGaragem/PrototipoGaragem.ino
+++ PrototipoGaragem/PrototipoGaragem.ino
@@ -1,12 +1,66 @@
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <Servo.h> 
+
+#define SERVO 3 // O servo está na porta digital 3
+#define GARAGEBUTTON 5 // O botão de comando do portão está na porta digital 5
+#define LIGHTBUTTON 4 // O botão de comando da led está na porta digital 4
+#define LIGHT 6 // O led que simula a lampada está na porta 6
+#define OFF LOW
+#define ON HIGH
+#define OPEN HIGH
+#define CLOSED LOW
+
+Servo portao;
+
+int lightStatus = OFF;
+int portaoStatus = CLOSED;
+int botaoGaragemStatus = LOW;
+int botaoLuzStatus = LOW;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(GARAGEBUTTON, INPUT);
+  pinMode(LIGHTBUTTON, INPUT);
+  pinMode(LIGHT, OUTPUT);
+
+  setupPortao(SERVO);
+  
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  int botaoGaragem = digitalRead(GARAGEBUTTON);
+  int botaoLuz = digitalRead(LIGHTBUTTON);
+  
+  if((botaoLuz != botaoLuzStatus)&&(botaoLuz)){
+    comutaLuz();
+  }
+  botaoLuzStatus = botaoLuz;
+
+}
+
+void comutaLuz(){
+  if(lightStatus){
+    digitalWrite(LIGHT, OFF);
+    lightStatus = OFF;
+  }
+  else{
+    digitalWrite(LIGHT, ON);
+    lightStatus = ON;
+  }
+}
+
+void acendeLuz(){
+  digitalWrite(LIGHT, ON);
+  lightStatus = ON;
+}
+
+void apagaLuz(){
+  digitalWrite(LIGHT, OFF);
+  lightStatus = OFF;
 }
 
 void setupPortao(int pin){
