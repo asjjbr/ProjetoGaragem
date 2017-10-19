ca8fbb1f57dc59a5c4dbb0b8a513c4000b929392
Sidnei de Souza Martins <ssmartins@gmail.com>
2017-10-19 21:54:17 -0200

    Implementado os leds de status do mqtt e o comando via mqtt

--- PrototipoGaragem/PrototipoGaragem.ino
+++ PrototipoGaragem/PrototipoGaragem.ino
@@ -1,7 +1,10 @@
+#define MQTT_SOCKET_TIMEOUT 5
+
 #include <SPI.h>
-//#include <UIPEthernet.h>
-//#include <utility/logging.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
 #include <Servo.h> 
+#include <PubSubClient.h>
 
 #define SERVO 3 // O servo está na porta digital 3
 #define GARAGEBUTTON 5 // O botão de comando do portão está na porta digital 5
@@ -36,6 +39,14 @@
 // Tempo de abertura/fechamento do portão
 #define GATETIME 10000
 
+
+#define CONNECTED 0x00
+#define DISCONNECTED 0x01
+
+
+// Update these with values suitable for your network.
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0xff};
+
 Servo gate; // Varíável de controle do servo motor
 int position = 0; // Posição do servo motor
 
@@ -44,11 +55,60 @@ int gateStatus = CLOSED;
 int botaoGaragemStatus = LOW;
 int botaoLuzStatus = LOW;
 
+int mqttPortao = 0;
+int mqttLuz = 0;
+
 int garageMachineState = INITIAL;
 unsigned long initialhour = 0;
 
 int gateMachineState = STOPED;
 unsigned long initialhourgate = 0;
+
+int mqttMachineState;
+
+int blinkLed = LOW;
+int contador = 30;
+unsigned long tempo = 30;
+unsigned long tempoinicial = 0;
+int blinkLedStatus = LOW;
+
+// Callback function header
+void callback(char *topic, byte *payload, unsigned int length);
+
+EthernetClient ethClient;
+
+// Dados do MQTT Cloud
+PubSubClient client("m10.cloudmqtt.com", 16367, callback, ethClient);
+
+// Funcçao que irá receber o retorno do servidor.
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  //Serial.println(*payload);
+  Serial.println(topic); 
+  String teste = String(topic); 
+  if(teste == "portao"){
+     Serial.println("comando mqtt portao");
+    if(payload[0] == 49){
+      mqttPortao = 1;
+       mqttLuz = 1;
+    }
+    else
+      mqttPortao = 0;
+  }
+
+  if(teste == "lampada"){
+    //if(payload[0] == 49){
+      Serial.println("comando mqtt lampada");
+      mqttLuz = 1;
+   // }
+   // else
+    //  mqttLuz = 0;
+  }
+
+  byte *p = (byte *)malloc(length);
+  memcpy(p, payload, length);
+  free(p);
+}
 
 void setup() {
   pinMode(GARAGEBUTTON, INPUT);
@@ -56,18 +116,36 @@ void setup() {
   pinMode(LIGHT, OUTPUT);
   pinMode(STATUSLEDRED, OUTPUT);
   pinMode(STATUSLEDGREEN, OUTPUT);
-  pinMode(STATUSLEDRED, OUTPUT);
+  pinMode(STATUSLEDBLUE, OUTPUT);
 
   setupGate(SERVO);
   
   Serial.begin(9600);
+  //Serial.println("Iniciando...");
+  Ethernet.begin(mac);
+  if (client.connect("Magal", "coiktbwj", "zAhaklL2atGf"))
+  {
+    // Envia uma mensagem para o cloud no topic portao
+    //client.publish("portao", 65);
+
+    // Conecta no topic para receber mensagens
+    client.subscribe("portao");
+    client.subscribe("lampada");
+    
+  
+    mqttMachineState = CONNECTED;
+  }else{
+    mqttMachineState = DISCONNECTED;
+  }
 
 }
 
 void loop() {
 
-  int botaoGaragem = digitalRead(GARAGEBUTTON); // Verifica se existe um comando de acionamento do portão
-  int botaoLuz = digitalRead(LIGHTBUTTON); // Verifica se existe um comando de acionamento da luz
+  client.loop();
+  
+  int botaoGaragem = comandoPortao();// digitalRead(GARAGEBUTTON); // Verifica se existe um comando de acionamento do portão
+  int botaoLuz = comandoLuz();//digitalRead(LIGHTBUTTON); // Verifica se existe um comando de acionamento da luz
 
   // Se houver comando de acionamento da luz, o estado dela é comutado (acende se estiver apagada, apaga se estiver acesa)
   if((botaoLuz != botaoLuzStatus)&&(botaoLuz)){
@@ -150,6 +228,52 @@ void loop() {
       }
       break;
   }
+
+  switch(mqttMachineState){
+    case CONNECTED:
+      acendeLed(STATUSLEDGREEN);
+      apagaLed(STATUSLEDRED);
+      if(!client.connected()){
+        mqttMachineState = DISCONNECTED;
+      }
+      break;
+    case DISCONNECTED:
+      apagaLed(STATUSLEDGREEN);
+      acendeLed(STATUSLEDRED);
+      if (client.connect("Magal", "coiktbwj", "zAhaklL2atGf")){
+        // Conecta no topic para receber mensagens
+        client.subscribe("portao");
+        client.subscribe("lampada");
+    
+        mqttMachineState = CONNECTED;
+      }
+      else{
+        mqttMachineState = DISCONNECTED;
+      }
+      break;
+  }
+
+  if(blinkLed){
+    if(!contador){
+      blinkLed = LOW;
+      contador = 100;
+      apagaLed(STATUSLEDBLUE);
+    }
+    else{
+      if(millis() - tempoinicial > tempo){
+        if(blinkLedStatus){
+          apagaLed(STATUSLEDBLUE);
+          blinkLedStatus = LOW;
+        }
+        else{
+          acendeLed(STATUSLEDBLUE);
+          blinkLedStatus = HIGH;
+        }
+        tempoinicial = millis();
+        contador--;
+      }
+    }
+  }
 }
 
 // A função checa qual o estado atual da luz da garagem e o alterna (se estiver desligado, liga. se estiver ligado, desliga)
@@ -162,6 +286,8 @@ void comutaLuz(){
     digitalWrite(LIGHT, ON);
     lightStatus = ON;
   }
+  blinkLed = HIGH;
+  tempoinicial = millis();
 }
 
 // A função acende a luz da garagem
@@ -201,3 +327,30 @@ int isGarageOPEN(){
   return gateStatus;
 }
 
+void acendeLed(int pin){
+  digitalWrite(pin, HIGH);
+}
+void apagaLed(int pin){
+  digitalWrite(pin, LOW);
+}
+
+int comandoPortao(){
+  int botao = digitalRead(GARAGEBUTTON);
+  botao |= mqttPortao;
+  mqttPortao = 0;
+  return botao;
+}
+
+int comandoLuz(){
+  int botao = digitalRead(LIGHTBUTTON);
+  if(mqttLuz){
+    Serial.print("botao ");
+  
+  Serial.println(botao);
+  Serial.print("mqtt ");
+  Serial.println(mqttLuz);
+  }
+  botao |= mqttLuz;
+  mqttLuz = 0;
+  return botao; 
+}
