1d6803822cc377e6c1c89332298d66c1899451f4
asjjbr <ssmartins@gmail.com>
2017-10-18 23:11:23 -0200

    Criada as funções de abertura e fechamento do portão, criada função para testar se o portão está aberto ou fechado. Código comentado até a linha 92

--- PrototipoGaragem/PrototipoGaragem.ino
+++ PrototipoGaragem/PrototipoGaragem.ino
@@ -7,24 +7,58 @@
 #define GARAGEBUTTON 5 // O botão de comando do portão está na porta digital 5
 #define LIGHTBUTTON 4 // O botão de comando da led está na porta digital 4
 #define LIGHT 6 // O led que simula a lampada está na porta 6
-#define OFF LOW
-#define ON HIGH
-#define OPEN HIGH
-#define CLOSED LOW
+#define STATUSLEDRED 7 // O led vermelho de status da conexão MQTT está no pino 7
+#define STATUSLEDGREEN 9 // O led verde de status da conexão MQTT está no pino 9
+#define STATUSLEDBLUE 8 // O led azul de status da conexão MQTT está no pino 8
 
-Servo portao;
+#define OFF LOW // Mapeamento do texto OFF como nivel baixo
+#define ON HIGH // Mapeamento do texto ON como nivel alto
+#define OPENED HIGH // Mapeamento do texto OPENED como nivel alto
+#define CLOSED LOW // Mapeamento do texto CLOSED como nivel baixo
+
+// Valores possíveis da máquina de estados de controle da garagem
+#define INITIAL 0x00
+#define GATECLOSED 0x01
+#define gateOPENING 0x02
+#define GATEOPENED 0x04
+#define GATECLOSING 0x08
+#define gateRECEMABERTO 0x10
+#define GATEJUSTCLOSED 0x20
+
+// Valors possíveis da máquina de estados de controle do portão
+#define STOPED 0x00
+#define OPENING 0x01
+#define CLOSING 0x02
+
+// Tempo gasto entre o portão abrir e a luz acender ou o portão fechar e a luz apagar
+#define TEMPOLUZ 1000
+
+// Tempo de abertura/fechamento do portão
+#define GATETIME 10000
+
+Servo gate; // Varíável de controle do servo motor
+int position = 0; // Posição do servo motor
 
 int lightStatus = OFF;
-int portaoStatus = CLOSED;
+int gateStatus = CLOSED;
 int botaoGaragemStatus = LOW;
 int botaoLuzStatus = LOW;
+
+int garageMachineState = INITIAL;
+unsigned long initialhour = 0;
+
+int gateMachineState = STOPED;
+unsigned long initialhourgate = 0;
 
 void setup() {
   pinMode(GARAGEBUTTON, INPUT);
   pinMode(LIGHTBUTTON, INPUT);
   pinMode(LIGHT, OUTPUT);
+  pinMode(STATUSLEDRED, OUTPUT);
+  pinMode(STATUSLEDGREEN, OUTPUT);
+  pinMode(STATUSLEDRED, OUTPUT);
 
-  setupPortao(SERVO);
+  setupGate(SERVO);
   
   Serial.begin(9600);
 
@@ -32,14 +66,90 @@ void setup() {
 
 void loop() {
 
-  int botaoGaragem = digitalRead(GARAGEBUTTON);
-  int botaoLuz = digitalRead(LIGHTBUTTON);
-  
+  int botaoGaragem = digitalRead(GARAGEBUTTON); // Verifica se existe um comando de acionamento do portão
+  int botaoLuz = digitalRead(LIGHTBUTTON); // Verifica se existe um comando de acionamento da luz
+
+  // Se houver comando de acionamento da luz, o estado dela é comutado (acende se estiver apagada, apaga se estiver acesa)
   if((botaoLuz != botaoLuzStatus)&&(botaoLuz)){
     comutaLuz();
   }
   botaoLuzStatus = botaoLuz;
 
+  switch(garageMachineState){
+    case GATECLOSED:
+      if(botaoGaragem){ // Se houver comando de acionamento do portão, muda o estado das máquinas de estado para abertura do portão
+        gateMachineState = OPENING;
+        garageMachineState = gateOPENING;
+      }
+      break;
+    case gateOPENING:
+      if (isGarageOPEN()){ // Se o portão estiver aberto, muda o estado da máquina de estado para portão recém aberto
+        garageMachineState = gateRECEMABERTO;
+        initialhour = millis();
+      }
+      break;
+    case gateRECEMABERTO:
+      if(millis() - initialhour > TEMPOLUZ){ // Se tiver decorrido TEMPOLUZ milissegundos da abertura do portão, acende a luz
+        garageMachineState = GATEOPENED;
+        switchOnLight();
+      }
+      else{
+        if(botaoGaragem){
+          gateMachineState = CLOSING;
+          garageMachineState = GATECLOSING;
+        }
+      }
+      break;
+    case GATEOPENED:
+      if(botaoGaragem){
+        gateMachineState = CLOSING;
+        garageMachineState = GATECLOSING;
+      }
+      break;
+    case GATECLOSING:
+      if (!isGarageOPEN()){
+        garageMachineState = GATEJUSTCLOSED;
+        initialhour = millis();
+      }
+      break;
+    case GATEJUSTCLOSED:
+      if(millis() - initialhour > TEMPOLUZ){
+        garageMachineState = GATECLOSED;
+        switchOffLight();
+      }
+      else{
+        if(botaoGaragem){
+          gateMachineState = OPENING;
+          garageMachineState = gateOPENING;
+        }
+      }
+      break;
+    case INITIAL:
+      if(isGarageOPEN()){
+        garageMachineState = GATEOPENED;
+      }
+      else{
+        garageMachineState = GATECLOSED;
+      }
+  }
+
+  switch(gateMachineState){
+    case STOPED:
+      initialhourgate = millis();
+      break;
+    case OPENING:
+      openGate();
+      if(isGarageOPEN()){
+        gateMachineState = STOPED;
+      }
+      break;
+    case CLOSING:
+      closeGate();
+      if(!isGarageOPEN()){
+        gateMachineState = STOPED;
+      }
+      break;
+  }
 }
 
 // A função checa qual o estado atual da luz da garagem e o alterna (se estiver desligado, liga. se estiver ligado, desliga)
@@ -55,25 +165,39 @@ void comutaLuz(){
 }
 
 // A função acende a luz da garagem
-void acendeLuz(){
+void switchOnLight(){
   digitalWrite(LIGHT, ON);
   lightStatus = ON;
 }
 
 // A função apaga a luz da garagem
-void apagaLuz(){
+void switchOffLight(){
   digitalWrite(LIGHT, OFF);
   lightStatus = OFF;
 }
 
-void setupPortao(int pin){
-  
+void setupGate(int pin){
+  gate.attach(pin);
+  gate.write(0);
+  position = 0;
 }
-void abrePortao(){
-
-  
+void openGate(){
+  position = (90*(millis()-initialhourgate))/GATETIME;
+  if(position>90){
+    position = 90;
+    gateStatus = OPENED;
+  }
+  gate.write(position);
 }
-void fechaPortao(){
-  
+void closeGate(){
+  position = 90-(90*(millis()-initialhourgate))/GATETIME;
+  if(position<0){
+    position = 0;
+    gateStatus = CLOSED;
+  }
+  gate.write(position);
+}
+int isGarageOPEN(){
+  return gateStatus;
 }
 
